66669cfc7d5094ff6c42af816ea51595d7faaccc
ireneusz <ireneusz.petryszak@gmail.com>
2021-04-23 10:29:48 +0200

    added api methods to handle courses n authors

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -23,10 +23,13 @@ module.exports = {
     https: false
   },
   plugins: [
+    new webpack.DefinePlugin({
+      "process.env.API_URL": JSON.stringify("http://localhost:3001")
+    }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
       favicon: "src/favicon.ico"
-    })
+    }),
   ],
   module: {
     rules: [
